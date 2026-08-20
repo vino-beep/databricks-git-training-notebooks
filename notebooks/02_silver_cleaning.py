4bf56fb725c2939191dba2daed3f373fbc7c2f15
vinotha <vinotha30oct@gmail.com>
2026-08-20 14:55:07 +0530

    Add silver customer cleaning

---
+++ notebooks/02_silver_cleaning.py
@@ -0,0 +1,19 @@
+# Databricks notebook source 
+
+bronze_df = spark.table( 
+
+   "data_engineering.bronze.customers" 
+
+) 
+
+silver_df = ( 
+
+   bronze_df 
+
+   .dropDuplicates(["customer_id"]) 
+
+   .dropna(subset=["customer_id"]) 
+
+) 
+
+display(silver_df) 
